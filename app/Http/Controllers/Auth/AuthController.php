337fcbf998a717cdf11511570c9ddc92dea55307
karakasm <turabkarakas@gmail.com>
2023-03-02 13:40:51 +0300

    Logout process created.

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -4,12 +4,15 @@ namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginPostRequest;
+use App\Models\User;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Session;
 
 class AuthController extends Controller
 {
     public function index()
     {
+
         return view('login');
     }
 
@@ -21,13 +24,34 @@ class AuthController extends Controller
 
             $request->session()->regenerate();
 
+            $user = Auth::user();
+            Session::put('user', [
+                'user_id' => $user->id,
+                'name' => $user->name,
+                'surname' => $user->surname,
+                'username' => $user->username,
+                'role' => $user->role,
+            ]);
+
             if (Auth::user()->role === 'instructor') {
                 return redirect()->route('instructor.index');
             } elseif (Auth::user()->role === 'student') {
                 return redirect()->route('student.index');
             }
         } else {
-            return redirect()->route('login')->withErrors(['deneme' => 'Kullanıcı Adınız veya Şifreniz Hatalı!']);
+            Session::flash('error','Kullanıcı Adınız veya Şifreniz Hatalı! Lütfen tekrar deneyiniz...');
+            return redirect()->route('login');
         }
+    }
+
+    public function handleLogout()
+    {
+        Auth::logout();
+
+        Session::flush();
+
+        Session::regenerate();
+
+        return redirect('/');
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', [AuthController::class,'index'])->name('login');
 Route::post('login',[AuthController::class,'handleLogin'])->name('handle.login');
+Route::get('logout',[AuthController::Class,'handleLogout'])->middleware('auth')->name('handle.logout');
+
 
 Route::view('student/index','student.index')->middleware('auth')->name('student.index');
 Route::view('instructor/index', 'instructor.index')->middleware('auth')->name('instructor.index');
